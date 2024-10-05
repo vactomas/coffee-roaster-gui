281e106f4074ad3667700e0998aa4f2f9a944ed5
Tomáš Vacek <vacek.tomas@weaport.eu>
2024-10-05 16:21:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+### Coffea Roaster App ###
+
+# imports
+import sqlite3
+import matplotlib.pyplot as plt
+import time
+
+
+def main():
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
